29255afd43d83aecbf56b58276609fa80aa34e52
AnandaRezaKurniawan <72422050+AnandaRezaKurniawan@users.noreply.github.com>
2021-11-11 10:48:09 +0700

    Add files via upload

---
+++ Pertemuan 2 PBB/Project/Modul2/app/src/main/java/com/anandarezakurniawan_19104061/myfirstapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.anandarezakurniawan_19104061.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
